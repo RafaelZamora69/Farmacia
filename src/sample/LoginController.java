a75fca56503372a18f6a550e461ca4dd5a089564
Rafael Zamora <rafael_zamora99@hotmail.com>
2019-09-12 13:58:12 -0500

    Login

---
+++ src/sample/LoginController.java
@@ -0,0 +1,21 @@
+package sample;
+
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+
+public class LoginController {
+    @FXML
+    private javafx.scene.layout.StackPane StackPane;
+
+    @FXML
+    private javafx.scene.layout.AnchorPane AnchorPane;
+
+    @FXML
+    private JFXTextField TxtUser;
+
+    @FXML
+    private JFXPasswordField TxtPass;
+
+    @FXML
+    private JFXButton BtnEntrar;
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,8 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
