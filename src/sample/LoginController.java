87cc52fc977c2f14d4c0e7c4bcc9087b245b82e1
Rafael Zamora <rafael_zamora99@hotmail.com>
2019-09-12 22:56:53 -0500

    PUTA MADRE

--- src/sample/LoginController.java
+++ src/sample/LoginController.java
@@ -1,20 +1,27 @@
 package sample;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 
 public class LoginController {
     @FXML
-    private javafx.scene.layout.StackPane StackPane;
+    private StackPane StackPane;
 
     @FXML
-    private javafx.scene.layout.AnchorPane AnchorPane;
+    private AnchorPane AnchorPane;
 
     @FXML
     private JFXTextField TxtUser;
 
     @FXML
     private JFXPasswordField TxtPass;
+
+    @FXML
+    private JFXButton BtnSalir;
 
     @FXML
     private JFXButton BtnEntrar;
