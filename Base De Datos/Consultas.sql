790b0abad92798b6dce58239e419045ab78480b8
Rafael Zamora <rafael_zamora99@hotmail.com>
2019-09-24 13:10:18 -0500

    Login

--- Base De Datos/Consultas.sql
+++ Base De Datos/Consultas.sql
@@ -3,8 +3,11 @@ use Farmacia;
 /* Insertar en Farmacia.Empleado */
 insert into Empleado (Nombre, Telefono, Usuario, Password, Jerarquia) values ('Gerardo Galván Chavez', '229 902 5028', 'GerardoGalvan', sha1('Gerardo123'), 1);
 insert into Empleado (Nombre, Telefono, Usuario, Password, Jerarquia) values ('Rafael Antonio Gonzalez Zamora', '229 524 2553', 'RafaelZamora'. sha1('Rafael123'), 1);
-insert into Empleado (Nombre, Telefono, Usuario, Password, Jerarquia) values ('', '', 'RafaelZamora'. sha1(), 1);
-insert into Empleado (Nombre, Telefono, Usuario, Password, Jerarquia) values ('', '', 'RafaelZamora'. sha1(), 1);
+insert into Empleado (Nombre, Telefono, Usuario, Password, Jerarquia) values ('', '', ''. sha1(), 1);
+insert into Empleado (Nombre, Telefono, Usuario, Password, Jerarquia) values ('', '', ''. sha1(), 1);
+
+/* Insertar en Farmacia.Proveedores */
+insert into Proveedor (Nombre, Telefono, Correo, Direccion) values ('Pepsico', '229 112 7509', 'evolucion_adm@hotmail.com', 'No se je');
 
 /* Insertar en Farmacia.Categorias */
 insert into Categorias (idCategoria, Descripcion) values (1, 'Refrescos / Bebidas');
@@ -17,13 +20,16 @@ insert into Presentacion (idPresentacion, Descripcion) values (1, 'Lata');
 insert into Presentacion (idPresentacion, Descripcion) values (2, 'Botella');
 insert into Presentacion (idPresentacion, Descripcion) values (3, 'Tabletas');
 
-/* Insertar en Farmacia.Proveedores */
-insert into Proveedor (Nombre, Telefono, Correo, Direccion) values ('Pepsico', '229 112 7509', 'evolucion_adm@hotmail.com', 'No se je');
-
 /* Insertar en Farmacia.Productos */
 insert into Producto (Cod_Barras, Descripcion, Presentación, Proveedor, Precio_Compra, Precio_Venta, Cantidad, Receta, idCategoria) 
-    values ('7501031311309', 'Pepsi 355ML', 1, 'Pepsico', 8.0, 10.0, 0, 0, 1);
+    values ('7501031311309', 'Pepsi 355ML', 1, 1, 8.0, 10.0, 0, 0, 1);
 insert into Producto (Cod_Barras, Descripcion, Presentación, Proveedor, Precio_Compra, Precio_Venta, Cantidad, Receta, idCategoria)
-    values ('7501031311606', 'Pepsi 2L', 2, 'Pepsico', 12.0, 15.0, 0, 0, 1);
+    values ('7501031311606', 'Pepsi 2L', 2, 1, 12.0, 15.0, 0, 0, 1);
 insert into Producto (Cod_Barras, Descripcion, Presentación, Proveedor, Precio_Compra, Precio_Venta, Cantidad, Receta, idCategoria)
-    values ('036731501004', 'Gatorade Naranja 500ML', 2, 'Pepsico', 10.0, 13.0, 0, 0, 1);
+    values ('036731501004', 'Gatorade Naranja 500ML', 2, 1, 10.0, 13.0, 0, 0, 1);
+
+/* Insertar en Farmacia.Cliente */
+insert into Cliente (Nombre, Direccion, Telefono, Edad, Puntos, Rfc) values ('Abelardo Hernandez Mota', 'Su casa', '229 368 4747', '20', 0)
+insert into Cliente (Nombre, Direccion, Telefono, Edad, Puntos, Rfc) values ('Jorge Antonio Pedroza Rendón', 'Su casa', '229 137 0546', '20', 0)
+insert into Cliente (Nombre, Direccion, Telefono, Edad, Puntos, Rfc) values ('', 'Su casa', '', '20', 0)
+insert into Cliente (Nombre, Direccion, Telefono, Edad, Puntos, Rfc) values ('', 'Su casa', '', '20', 0)
--- src/sample/LoginController.java
+++ src/sample/LoginController.java
@@ -1,11 +1,26 @@
 package sample;
 
+import Objetos.Conexion;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.security.Principal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class LoginController {
     @FXML
@@ -25,4 +40,30 @@ public class LoginController {
 
     @FXML
     private JFXButton BtnEntrar;
+
+    public void Ingresar(javafx.scene.input.MouseEvent mouseEvent) {
+        try{
+            Connection con = Conexion.getConnection();
+            PreparedStatement statement;
+            statement = con.prepareStatement("select idEmpleado from Empleado where Usuario = ? and Password = sha1(?)");
+            statement.setString(1, this.TxtUser.getText());
+            statement.setString(2, this.TxtPass.getText());
+            ResultSet rs = statement.executeQuery();
+            int Resultado = 0;
+            while (rs.next()){
+                //Cosas je
+                Resultado = 1;
+            }
+            if(Resultado == 1){
+                //Entrar a el main
+                Stage Main = new Stage();
+                Parent Mroot = FXMLLoader.load(getClass().getResource("Principal.fxml"));
+                Scene scene = new Scene(Mroot);
+                Main.setScene(scene);
+                Main.show();
+            }
+        } catch (SQLException | IOException ex){
+            Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
