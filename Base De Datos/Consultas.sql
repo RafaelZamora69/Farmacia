a354953eb237b22b9a4660c34eb44731036cccf9
Rafael Zamora <rafael_zamora99@hotmail.com>
2019-09-24 22:01:48 -0500

    Medicinas
    Unur

--- Base De Datos/Consultas.sql
+++ Base De Datos/Consultas.sql
@@ -7,13 +7,17 @@ insert into Empleado (Nombre, Telefono, Usuario, Password, Jerarquia) values ('K
 insert into Empleado (Nombre, Telefono, Usuario, Password, Jerarquia) values ('Alejandro Vega Hernandez', '5540907400', 'AlexVega', sha1('Vega123'), 1);
 
 /* Insertar en Farmacia.Proveedores */
-insert into Proveedor (Nombre, Telefono, Correo, Direccion) values ('Pepsico', '229 112 7509', 'evolucion_adm@hotmail.com', 'No se je');
+insert into Proveedor (Nombre, Telefono, Correo, Direccion) values ('Pepsico', '229 112 7509', 'evolucion_adm@hotmail.com', 'Carretera Veracruz Medellin KM 2.3');
+insert into Proveedor (Nombre, Telefono, Correo, Direccion) values ('Laboratorios PiSA', '229 921 8654', '', 'Ejército Mexicano, Adalberto Tejada, Veracruz');
+insert into Proveedor (Nombre, Telefono, Correo, Direccion) values ('AMSA Laboratorios', '55 5998 2100', '', 'Flores 56, Amp Candelaria, 04380 Ciudad de México');
+insert into Proveedor (Nombre, Telefono, Correo, Direccion) values ('Novag Infancia', '55 5666 4120', '', 'Calz. de Tlalpan 3417, Sta. Úrsula Coapa, 04650 Ciudad de México');
 
 /* Insertar en Farmacia.Categorias */
 insert into Categorias (idCategoria, Descripcion) values (1, 'Refrescos / Bebidas');
 insert into Categorias (idCategoria, Descripcion) values (2, 'Analgelsicos');
 insert into Categorias (idCategoria, Descripcion) values (3, 'Laxante');
 insert into Categorias (idCategoria, Descripcion) values (4, 'Antialérgicos');
+insert into Categorias (idCategoria, Descripcion) values (5, 'Genéricos');
 
 /* Insertar en Farmacia.Presentación */ 
 insert into Presentacion (idPresentacion, Descripcion) values (1, 'Lata');
@@ -33,7 +37,12 @@ insert into Producto (Cod_Barras, Descripcion, Presentación, Proveedor, Precio_
     values ('7501086801046', 'Agua Natural Epura 1LT', 2, 1, 8.00, 10.00, 0, 0, 1);
 insert into Producto (Cod_Barras, Descripcion, Presentación, Proveedor, Precio_Compra, Precio_Venta, Cantidad, Receta, idCategoria)
     values ('7501031360024', 'Manzanita Sol 600ML', 2, 1, 9.00, 11.50, 0, 0, 1);
-    
+insert into Producto (Cod_Barras, Descripcion, Presentación, Proveedor, Precio_Compra, Precio_Venta, Cantidad, Receta, idCategoria)
+    values ('7501349029880', 'Tramadol, Paracetamol 325 mg.', 3, 2, 90.0, 125.0, 0, 0, 2); 
+insert into Producto (Cod_Barras, Descripcion, Presentación, Proveedor, Precio_Compra, Precio_Venta, Cantidad, Receta, idCategoria)
+    values ('7502223703735', 'Ketorolaco 30 Mg.', 3, 3, 25.0, 31.0, 0, 1, 2);
+insert into Producto (Cod_Barras, Descripcion, Presentación, Proveedor, Precio_Compra, Precio_Venta, Cantidad, Receta, idCategoria)
+    values ('7501075722253', 'Nineka neomicina, Caolín y Pectina', 3, 4, 40.0, 60.0, 0, 0, 2, 5);
     
 
 /* Insertar en Farmacia.Cliente */
