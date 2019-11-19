43c4e0753c27065f0b22d314550455d388c5cdc3
Rafael Zamora <rafael_zamora99@hotmail.com>
2019-11-18 23:09:49 -0600

    Extras Uwur

--- src/sample/ExtrasController.java
+++ src/sample/ExtrasController.java
@@ -11,9 +11,11 @@ import javafx.scene.control.TreeTableColumn;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import tray.notification.NotificationType;
 
 import java.net.URL;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
@@ -36,7 +38,7 @@ public class ExtrasController implements Initializable {
     private JFXTextField EusuEmp;
 
     @FXML
-    private JFXComboBox<?> EPuestoEmp;
+    private JFXComboBox<String> EPuestoEmp;
 
     @FXML
     private JFXPasswordField EValidarPass;
@@ -72,7 +74,7 @@ public class ExtrasController implements Initializable {
     private JFXPasswordField NValidarPass;
 
     @FXML
-    private JFXComboBox<?> NPuestoEmp;
+    private JFXComboBox<String> NPuestoEmp;
 
     @FXML
     private Label ValidarPass;
@@ -83,8 +85,25 @@ public class ExtrasController implements Initializable {
     @FXML
     private JFXButton BtnAgregar;
 
+    @FXML
+    private JFXButton BtnActualizar;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        CargarComboBox();
+    }
+
+    private void CargarComboBox(){
+        try {
+            Connection con = Conexion.getConnection();
+            ResultSet rs = con.createStatement().executeQuery("select distinct Jerarquia from Empleado;");
+            while(rs.next()){
+                this.EPuestoEmp.getItems().add(rs.getString(1));
+                this.NPuestoEmp.getItems().add(rs.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -92,13 +111,16 @@ public class ExtrasController implements Initializable {
         if(keyEvent.getCode().equals(KeyCode.ENTER)){
             if(!"".equals(this.BuscarEmp.getText())){
                 try {
+                    ValidarPass.setText("");
                     Connection con = Conexion.getConnection();
-                    ResultSet rs = con.createStatement().executeQuery("select Nombre, Telefono, Usuario, Jerarquia from Empleado where idEmpleado = ?;");
+                    PreparedStatement statement = con.prepareStatement("select Nombre, Telefono, Usuario, Jerarquia from Empleado where idEmpleado = ?;");
+                    statement.setString(1, this.BuscarEmp.getText());
+                    ResultSet rs = statement.executeQuery();
                     while(rs.next()){
                         this.ENombreEmp.setText(rs.getString(1));
                         this.ETelEmp.setText(rs.getString(2));
                         this.EusuEmp.setText(rs.getString(3));
-
+                        this.EPuestoEmp.setValue(rs.getString(4));
                     }
                 } catch (SQLException e) {
                     e.printStackTrace();
@@ -108,6 +130,69 @@ public class ExtrasController implements Initializable {
     }
 
     public void InsertarEmpleado(MouseEvent mouseEvent) {
+        try {
+            Connection con = Conexion.getConnection();
+            PreparedStatement statement = con.prepareStatement("insert into Empleado (Nombre, Telefono, Usuario, Password, Jerarquia) values (?, ?, ?, sha1(?), ?)");
+            statement.setString(1, NNombreEmp.getText());
+            statement.setString(2, NTelEmp.getText());
+            statement.setString(3, NUsuEmp.getText());
+            if(this.ValidarPass2.getText().equals("Correcto")){
+                statement.setString(4, NPassEmp.getText());
+            } else {
+                Alertas.MostrarAlerta("Las contraseñas no coinciden", NotificationType.ERROR, "Error!");
+                throw new SQLException();
+            }
+            statement.setString(5, NPuestoEmp.getValue());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void ActualizarEmpleado(MouseEvent mouseEvent) {
+        try {
+            Connection con = Conexion.getConnection();
+            PreparedStatement statement;
+            if(ValidarPass.getText().equals("")){
+                statement = con.prepareStatement("update Empleado set Nombre = ?, Telefono = ?, Puesto = ?;");
+                statement.setString(1, ENombreEmp.getText());
+                statement.setString(2, ETelEmp.getText());
+                statement.setString(3, EPuestoEmp.getValue());
+                statement.executeUpdate();
+                Alertas.MostrarAlerta("Empleado actualizado", NotificationType.SUCCESS, "Correcto");
+            } else {
+                statement = con.prepareStatement("update Empleado set Nombre = ?, Telefono = ?, Puesto = ?, Password = sha1(?);");
+                statement.setString(1, ENombreEmp.getText());
+                statement.setString(2, ETelEmp.getText());
+                statement.setString(3, EPuestoEmp.getValue());
+                if(ValidarPass.getText().equals("Correcto")){
+                    statement.setString(4, EPassEmp.getText());
+                } else {
+                    Alertas.MostrarAlerta("Las contraseñas no coinciden", NotificationType.ERROR, "Error!");
+                    throw new SQLException();
+                }
+                statement.executeUpdate();
+                Alertas.MostrarAlerta("Empleado actualizado", NotificationType.SUCCESS, "Correcto");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void ValidarPass(KeyEvent keyEvent) {
+        if (keyEvent.getSource().equals(NPassEmp)){
+            if(this.NValidarPass.getText().equals(this.NPassEmp.getText()) && this.NPassEmp.getText().equals(this.NValidarPass.getText())){
+                this.ValidarPass2.setText("Correcto");
+            } else {
+                this.ValidarPass2.setText("No coinciden");
+            }
+        } else if (keyEvent.getSource().equals(EPassEmp)){
+            if(this.EValidarPass.getText().equals(this.EPassEmp.getText()) && this.EPassEmp.getText().equals(this.EValidarPass.getText())){
+                this.ValidarPass.setText("Correcto");
+            } else {
+                this.ValidarPass.setText("No coinciden");
+            }
+        }
     }
 
     class Empleado extends RecursiveTreeObject<Empleado> {
